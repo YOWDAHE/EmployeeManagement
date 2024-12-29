acf1019ce96b42bd715fe2605a738c49581bfdd9
yodahe <ketemayodahe@gmail.com>
2024-12-29 13:36:34 +0300

    firebase fix

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA7pupm2xqAO74LeXkonxguszkkHZLc-E0",
-    authDomain: "employee-managment-a84ff.firebaseapp.com",
-    projectId: "employee-managment-a84ff",
-    storageBucket: "employee-managment-a84ff.appspot.com",
-    messagingSenderId: "250655974750",
-    appId: "1:250655974750:web:9014fd4cefd55472531bcf"
+  apiKey: "AIzaSyA7pupm2xqAO74LeXkonxguszkkHZLc-E0",
+  authDomain: "employee-managment-a84ff.firebaseapp.com",
+  databaseURL: "https://employee-managment-a84ff-default-rtdb.firebaseio.com",
+  projectId: "employee-managment-a84ff",
+  storageBucket: "employee-managment-a84ff.firebasestorage.app",
+  messagingSenderId: "250655974750",
+  appId: "1:250655974750:web:9014fd4cefd55472531bcf"
 };
 
 // Initialize Firebase
